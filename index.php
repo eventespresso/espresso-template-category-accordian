3ed734470e4745e34797a32c207b8b077c3cba7e
Seth <seth@eventespresso.com>
2013-10-23 15:12:18 -0600

    Adding PUE to category accordion. [touch: 3906]

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   Plugin Name: Event Espresso Template - Category Accordion
   Plugin URI: http://www.eventespresso.com
   Description: Will display the categories in bars, once clicked events associated with that category will appear in an "accordion" style. If category colours are turned on, the block to the left will be that colour, otherwise it will default to grey. [EVENT_CUSTOM_VIEW template_name="category-accordion"] (Extra parameter: exclude="1,2,3" This uses the category IDs and will exclude them from being listed. Use a single number or a comma separated list of numbers.)
-  Version: 1.0
+  Version: 1.0.p
   Author: Event Espresso
   Author URI: http://www.eventespresso.com
   Copyright 2013 Event Espresso (email : support@eventespresso.com)
@@ -166,5 +166,36 @@ if (!function_exists('espresso_category_accordion')) {
 	});
 	</script>
 	<?php
+	}
+}
+
+/**
+ * hook into PUE updates
+ */
+//Update notifications
+add_action('action_hook_espresso_template_category_accordion_update_api', 'espresso_template_category_accordion_load_pue_update');
+function espresso_template_category_accordion_load_pue_update() {
+	global $org_options, $espresso_check_for_updates;
+	if ( $espresso_check_for_updates == false )
+		return;
+		
+	if (file_exists(EVENT_ESPRESSO_PLUGINFULLPATH . 'class/pue/pue-client.php')) { //include the file 
+		require(EVENT_ESPRESSO_PLUGINFULLPATH . 'class/pue/pue-client.php' );
+		$api_key = $org_options['site_license_key'];
+		$host_server_url = 'http://eventespresso.com';
+		$plugin_slug = array(
+			'premium' => array('p'=> 'espresso-template-category-accordion'),
+			'prerelease' => array('b'=> 'espresso-template-category-accordion-pr')
+			);
+		$options = array(
+			'apikey' => $api_key,
+			'lang_domain' => 'event_espresso',
+			'checkPeriod' => '24',
+			'option_key' => 'site_license_key',
+			'options_page_slug' => 'event_espresso',
+			'plugin_basename' => plugin_basename(__FILE__),
+			'use_wp_update' => FALSE
+		);
+		$check_for_updates = new PluginUpdateEngineChecker($host_server_url, $plugin_slug, $options); //initiate the class and start the plugin update engine!
 	}
 }
