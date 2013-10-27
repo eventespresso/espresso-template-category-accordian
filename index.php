a9008811d1d0b9cd79565f536393ccc4539b3167
Seth <seth@eventespresso.com>
2013-10-27 09:42:35 -0600

    Fixing open spots and event status variables.

--- index.php
+++ index.php
@@ -90,7 +90,7 @@ if (!function_exists('espresso_category_accordion')) {
 					$ext = $pathinfo['extension'];
 					$path_to_thumbnail = $dirname . $filename . '.' . $ext;
 					$externalURL = $event->externalURL; $registration_url = !empty($externalURL) ? $externalURL : espresso_reg_url($event->id);
-	
+					$event_status = event_espresso_get_status($event->id);
 					if ( $pathinfo['dirname'] == $upload_dir['baseurl'] ) {
 						if ( ! file_exists( $uploads['basedir'] . DIRECTORY_SEPARATOR . $filename . '.' . $ext )) {
 							$path_to_thumbnail = file_exists( $uploads['basedir'] . DIRECTORY_SEPARATOR . $filename . '.' . $ext ) ? $event_meta['event_thumbnail_url'] : FALSE;
@@ -99,7 +99,11 @@ if (!function_exists('espresso_category_accordion')) {
 				}
 				
 				//lets check the staus and attendee count
-				$open_spots	= get_number_of_attendees_reg_limit($event->id, 'number_available_spaces');
+				if ( ! has_filter( 'filter_hook_espresso_get_num_available_spaces' ) ){
+					$open_spots		= apply_filters('filter_hook_espresso_get_num_available_spaces', $event->id); //Available in 3.1.37
+				}else{
+					$open_spots		= get_number_of_attendees_reg_limit($event->id, 'number_available_spaces');
+				}
 
 				if($open_spots < 1 && $event->allow_overflow == 'N') {
 					$link_text = __('Sold Out', 'event_espresso');
